5ef5b816ea4be8624ea4c18add7434ba0d202339
Rachit Shah <rshah240@gmail.com>
2020-08-14 14:23:18 +0530

    Changes for Integrated Module

--- cnn_visualiser/__init__.py
+++ cnn_visualiser/__init__.py
@@ -2,4 +2,5 @@ from .grad_cam import GRAD_CAM
 from .intermediate_activations import IntermediateActivations
 from .vanilla_gradients import VanillaGradients
 from .gradient_visualiser import  GradVisualiser
+from .integrated_gradients import IntegratedGradients
 
