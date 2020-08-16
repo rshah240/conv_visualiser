9e5d38cc92998c916af04b2972fc3ad1a07a0841
Rachit Shah <rshah240@gmail.com>
2020-08-16 13:08:02 +0530

    Tutorial File Updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name  = 'conv_visualiser',
-      version = '0.3',
+      version = '0.4',
       description= 'Functions to visualize intermediate activations and features of a CNN',
       packages = ['cnn_visualiser'],
       zip_safe = False)
